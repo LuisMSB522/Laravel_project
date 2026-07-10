88bbe0cad474e31d1f21444379e8caed2d6e71a8
LuisMSB522 <153326518+LuisMSB522@users.noreply.github.com>
2026-07-10 19:38:41 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+//llamando los controladores de productos
+use App\Http\Controllers\ProductoController;
+
+Route::get('/productos', [ProductoController::class, 'index'])->name('productos.index');    //salida visual
+Route::post('/productos', [ProductoController::class, 'store'])->name('productos.store');   //agregar un registro
+Route::put('/productos', [ProductoController::class, 'update'])->name('productos.update');  //modificar un registro
+
+
+//llamando los controladores de categoria
+use App\Http\Controllers\CategoriaController;
+
+Route::get('/categoria', [CategoriaController::class, 'index'])->name('categoria.index');   //salida visual
+Route::post('/categoria', [CategoriaController::class, 'store'])->name('categoria.store');  //almacenar
+Route::put('/categoria', [CategoriaController::class, 'update'])->name('categoria.update'); //modificar
